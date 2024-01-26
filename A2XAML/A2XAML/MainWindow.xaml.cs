6a67588478bf5046a67b55dcf03ec587e30fb94f
Huynh Hoang Phuc <hoangphuchuynh2003@gmail.com>
2024-01-26 16:47:05 +0700

    Add Face Maker design WPF

---
+++ A2XAML/A2XAML/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+
+namespace A2XAML
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnSelectHairStyle1(object sender, RoutedEventArgs e)
+        {
+            // Implement the logic for selecting Hair Style 1
+        }
+        private void OnSelectHairStyle2(object sender, RoutedEventArgs e)
+        {
+            // Implement the logic for selecting Hair Style 2
+        }
+        private void OnSelectEyeStyle1(object sender, RoutedEventArgs e)
+        {
+            // Implement the logic for selecting Eye Style 1
+        }
+        private void OnSelectEyeStyle2(object sender, RoutedEventArgs e)
+        {
+            // Implement the logic for selecting Eye Style 2
+        }
+        private void OnSelectNoseStyle1(object sender, RoutedEventArgs e)
+        {
+            // Implement the logic for selecting Nose Style 1
+        }
+        private void OnSelectNoseStyle2(object sender, RoutedEventArgs e)
+        {
+            // Implement the logic for selecting Nose Style 2
+        }
+        private void OnSelectMouthStyle1(object sender, RoutedEventArgs e)
+        {
+            // Implement the logic for selecting Mouth Style 1
+        }
+        private void OnSelectMouthStyle2(object sender, RoutedEventArgs e)
+        {
+            // Implement the logic for selecting Mouth Style 2
+        }
+        private void OnRandomFace(object sender, RoutedEventArgs e)
+        {
+            // Implement the logic for random face
+        }
+    }
+}

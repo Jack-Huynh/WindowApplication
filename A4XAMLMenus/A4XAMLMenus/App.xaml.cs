5661bd54c4900f99d83faf41b270e111e3e52e0f
Huynh Hoang Phuc <hoangphuchuynh2003@gmail.com>
2024-02-14 16:43:46 +0700

    Finish A4XAMLMenus

---
+++ A4XAMLMenus/A4XAMLMenus/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A4XAMLMenus
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
